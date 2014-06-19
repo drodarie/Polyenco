6be4d906a76b7f680035492493cd929a428ba899
PReynaud <pierre.reynaud@hotmail.fr>
2014-06-19 12:42:44 +0200

    Modifs carousel

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -79,10 +79,8 @@ BRUSHED.slider = function(){
 		thumb_links				:	0,			// Individual thumb links for each slide
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
-											{image : '../webroot/img/slider-images/image01.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
-											{image : '../webroot/img/slider-images/image02.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
-											{image : '../webroot/img/slider-images/image03.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
-											{image : '../webroot/img/slider-images/image04.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''}  
+											{image : '../webroot/img/slider/Lyon.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''},
+											{image : '../webroot/img/slider/Bureau.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''} 
 									],
 									
 		// Theme Options			   
