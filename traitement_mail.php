cf06a2b0b5dce17dcdec39a5dafc669df515c7db
Slashgear <antoine395.caron@laposte.net>
2014-09-21 17:34:58 +0200

    [ACN]:Modif Ancre retour fonction traitement mail

--- traitement_mail.php
+++ traitement_mail.php
@@ -19,18 +19,18 @@ if(isset($_POST['contact_nom'])&& isset($_POST['contact_prenom'])&& isset($_POST
 
     if (mail($destinataire,$subject,$body,$headers)) 
          { 
-              header('Location: index.php?envoi=0/#Contact');
+              header('Location: index.php?envoi=0#Contact');
                       exit();
          } 
          else 
          { 
-             header('Location: index.php?envoi=1/#Contact');
+             header('Location: index.php?envoi=1#Contact');
                       exit();
          } 
 }
 else
 { 
-              header('Location: index.php?envoi=1/#Contact');
+              header('Location: index.php?envoi=1#Contact');
                       exit();
 } 
 ?>
