a1ccafd5e16a08496f0450d2de56c9b94060cc8f
Slashgear <antoine395.caron@laposte.net>
2014-10-05 09:31:40 +0200

    Fusion

--- index.php
+++ index.php
@@ -83,7 +83,6 @@
                 a.src = g;
                 m.parentNode.insertBefore(a, m)
             })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
             ga('create', 'UA-43036950-1', 'auto');
             ga('require', 'displayfeatures');
             ga('send', 'pageview');
